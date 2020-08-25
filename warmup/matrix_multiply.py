013e54e3ee78d7205477caab88cf76363d9bb0fc
Jordan Boyd-Graber <jordanbg@gmail.com>
2020-08-25 12:52:59 -0400

    entry

--- warmup/matrix_multiply.py
+++ warmup/matrix_multiply.py
@@ -14,3 +14,35 @@ def matrix_multiply(left, right):
 
 
     return np.array([[5, 2], [3, 4]])
+
+def user_input_matrix():
+    R = int(input("Enter the number of rows:\t")) 
+    C = int(input("Enter the number of columns:\t"))
+
+    assert(R > 0), "Number of rows must be positive"
+    assert(C > 0), "Number of columns must be positive"
+  
+    # Initialize matrix 
+    matrix = [] 
+    print("Enter the entries rowwise:") 
+  
+    # For user input 
+    for i in range(R):          # A for loop for row entries 
+        a = [] 
+        for j in range(C):      # A for loop for column entries 
+             a.append(int(input("Row %i, Column %i:\t" % (i, j)))) 
+        matrix.append(a) 
+        
+    return np.array(matrix)
+
+if __name__ == "__main__":
+    print("Left matrix")
+    print("=====================")
+    left = user_input_matrix()
+    print("Right matrix")
+    print("=====================")    
+    right = user_input_matrix()
+
+    print("Product")
+    print("=====================")    
+    print(matrix_multiply(left, right))     
